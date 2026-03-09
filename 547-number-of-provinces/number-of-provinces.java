7aa20ffd6068236ca99870b011288445f3419180
Harsh Singh <harshsinghrajput895@gmail.com>
2026-03-09 15:19:24 +0530

    Time: 2 ms (46.04%) | Memory: 47.4 MB (42.80%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,30 +1,42 @@
 class Solution {
-    public void bfs (int i, int[][] isConnected, boolean [] visit){
-         int n=isConnected.length;
-        visit[i]=true;
-        Queue <Integer> q=new LinkedList<>();
-        q.add(i);
-       while(!q.isEmpty()){
-        int front=q.remove();
-        for(int j=0;j<n;j++){
-            if(isConnected[front][j]==1 && !visit[j]){
-                q.add(j);
-                visit[j]=true;
+    class DSU {
+        int parent[];
+
+        DSU(int n){
+            parent = new int[n];
+            for(int i=0;i<n;i++){
+                parent[i]=i;
             }
         }
-       }
+
+        public void union(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+            if (pa != pb) parent[pb] = pa;
+        }
+
+        public int find(int x) {
+            if (parent[x] == x) return x;
+            return parent[x] = find(parent[x]);
+        }
     }
+
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        int count=0;
-        boolean [] vist= new boolean[n];
-        for(int i=0;i< n;i++){
-            if(!vist[i]){
-                bfs(i,isConnected,vist);
-                count++;
+        int n = isConnected.length;
+        DSU dsu = new DSU(n);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(isConnected[i][j] == 1) {
+                    dsu.union(i, j);
+                }
             }
+        }
+
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(dsu.find(i) == i) count++;
         }
         return count;
-        
     }
 }
