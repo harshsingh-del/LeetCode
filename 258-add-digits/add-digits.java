337be455a6404b4fe5652e5f8b825179cb0b9393
Harsh Singh <harshsinghrajput895@gmail.com>
2026-03-26 22:27:13 +0530

    Time: 1 ms (98.07%) | Memory: 42.4 MB (87.52%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num==0 || num<=9) return num;
+        //  return 1+(num-1)%9;
+        if(num%9==0) return 9;
+         return num%9;
+    }
+}
